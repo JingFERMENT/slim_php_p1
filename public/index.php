9a5c2b2db46b8b21aed875b20d70aa07f038be15
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-08-27 11:52:11 +0200

    add a parameter

--- public/index.php
+++ public/index.php
@@ -8,13 +8,29 @@ use Slim\Factory\AppFactory;
 // load all the dependacies 
 require __DIR__ . '/../vendor/autoload.php';
 
-// create an app 
+// Instantiate App
 $app = AppFactory::create();
 
+// Define app routes
 
+// 1st route
 $app->get('/', function(Request $request, Response $response){
-    $response->getBody()->write("Bonjour, SlimPhP!");
+    $response->getBody()->write("Hello, SlimPhP!");
     return $response;
+});
+
+// 2nd route
+$app->get('/hello/jing', function(Request $request, Response $response){
+    $response->getBody()->write("Hello, Jing");
+    return $response;
+});
+
+// 3nd route by using a parameter
+// array $args = parameters 
+$app->get('/hello/{name}', function(Request $request, Response $response, array $args){
+   $name = ucfirst($args['name']);
+   $response->getBody()->write(sprintf("Hello, %s!", $name));
+   return $response;
 });
 
 $app->run();
